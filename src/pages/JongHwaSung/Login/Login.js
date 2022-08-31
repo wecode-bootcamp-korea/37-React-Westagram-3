c8235410006750e2f0fa7f6ef021a11a32a0584c
rayong <skdyds@gmail.com>
2022-08-31 15:42:21 +0900

    Add : 초기 세팅 완료

--- src/pages/JongHwaSung/Login/Login.js
+++ src/pages/JongHwaSung/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Login() {
-    return <h1>hello world</h1>
+function LoginSung() {
+  return <h1>hello world</h1>;
 }
 
-export default Login;
+export default LoginSung;
--- src/pages/jangmoonjung/Main/Main.js
+++ src/pages/jangmoonjung/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function Main() {
-    return <h1>hello world main</h1>
+function MainJang() {
+  return <h1>hello world main</h1>;
 }
 
-export default Main;
+export default MainJang;
