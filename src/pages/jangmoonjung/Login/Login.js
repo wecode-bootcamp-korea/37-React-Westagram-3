e2c1446be43ef2461752f557274fcb2f9495d363
gamangee <gamangee1991@gmail.com>
2022-09-01 16:55:57 +0900

    ADD: Login 페이지 기능 구현 - Mission 1 - 사용자 입력 데이터 저장 기능 구현 - Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jangmoonjung/Login/Login.js
+++ src/pages/jangmoonjung/Login/Login.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
 
 function LoginJang() {
-  return <h1>git branch feature/37-jangmoonjung</h1>;
+  // 입력 데이터 저장
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
+  function saveUserId(event) {
+    setIdValue(event.target.value);
+  }
+  function saveUserPw(event) {
+    setPwValue(event.target.value);
+  }
+  // 로그인 버튼 활성화
+  const [btnDisable, setBtnDisable] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+  const navigate = useNavigate();
+  const goToMain = () => {
+    if (!btnDisable) {
+      navigate('/jangmoonjung/Main');
+    }
+  };
+
+  return (
+    <main className="login">
+      <article className="container">
+        <h1 className="logo">Westagram</h1>
+        <form className="form">
+          <input
+            onChange={e => {
+              saveUserId(e);
+              if (idValue.includes('@') && pwValue.length > 5) {
+                setIsActive(true);
+                setBtnDisable(false);
+              } else {
+                setIsActive(false);
+                setBtnDisable(true);
+              }
+            }}
+            className="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            onChange={e => {
+              saveUserPw(e);
+              if (idValue.includes('@') && pwValue.length > 5) {
+                setIsActive(true);
+                setBtnDisable(false);
+              } else {
+                setIsActive(false);
+                setBtnDisable(true);
+              }
+            }}
+            className="pw"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <button
+            disabled={btnDisable}
+            onClick={goToMain}
+            type="button"
+            className={isActive ? 'btnActive' : 'btnUnactive'}
+          >
+            로그인
+          </button>
+        </form>
+        <span className="validation" />
+        <div className="find">비밀번호를 잊으셨나요?</div>
+      </article>
+    </main>
+  );
 }
 
 export default LoginJang;
