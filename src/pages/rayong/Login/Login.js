2ecf057379920b055046f074e83c2c0b0b1d6d37
rayong <skdyds@gmail.com>
2022-09-01 14:09:45 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/rayong/Login/Login.js
+++ src/pages/rayong/Login/Login.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../Login/Login.scss';
-// import { Link } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function LoginRa() {
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/rayong/Main');
+  };
+
+  const [userId, setUserId] = useState('');
+  const [userPW, setUserPW] = useState('');
+
+  const saveUserId = event => {
+    setUserId(event.target.value);
+  };
+
+  const saveUserPw = event => {
+    setUserPW(event.target.value);
   };
 
   return (
@@ -16,12 +27,14 @@ function LoginRa() {
         <form id="inputBox">
           <div className="input-box">
             <input
+              onChange={saveUserId}
               id="inputId"
               type="text"
               placeholder="@가 포함된 이메일 주소를 입력하세요!"
               autoComplete="username"
             />
             <input
+              onChange={saveUserPw}
               id="inputPw"
               type="password"
               placeholder="비밀번호는 다섯글자 이상입니다."
@@ -32,8 +45,6 @@ function LoginRa() {
             <button id="loginBtn" onClick={goToMain}>
               로그인
             </button>
-            {/* <Link to='/Main'><button id="loginBtn">로그인</button></Link> */}
-            {/* <button disabled id="loginBtn">로그인</button> */}
           </div>
         </form>
         <p className="info">
