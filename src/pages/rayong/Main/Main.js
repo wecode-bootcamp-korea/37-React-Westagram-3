b273f88e2d843d069912a40bc4f664742dc274bb
rayong <skdyds@gmail.com>
2022-09-05 16:04:57 +0900

    Modify : Nav 컴포넌트 공통 적용을 위해 다시 병함

--- src/pages/rayong/Main/Main.js
+++ src/pages/rayong/Main/Main.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import '../Main/Main.scss';
-import Nav from './components/Nav/Nav';
 import Feeds from './components/Feeds/Feeds';
 import MainRight from './components/MainRight/MainRight';
 
 function MainRa() {
   return (
     <div className="main">
-      <Nav />
+      <nav className="nav">
+        <div className="wrapper">
+          <div className="nav-box">
+            <div className="logo-box">
+              <img
+                className="icon"
+                src="/images/rayong/logo.png"
+                alt="westagram-logo"
+              />
+              <p>Westagram</p>
+            </div>
+            <div className="input-box">
+              <input id="search-inputbox" type="text" placeholder="검색" />
+              <i className="fa-solid fa-magnifying-glass" />
+            </div>
+            <div className="icon-box">
+              <i className="fa-regular fa-compass fa-xl" />
+              <i className="fa-regular fa-heart fa-xl" />
+              <i className="fa-regular fa-user fa-xl" />
+            </div>
+          </div>
+        </div>
+      </nav>
       <main className="contents">
         <div className="wrapper">
           <div className="contents-box">
