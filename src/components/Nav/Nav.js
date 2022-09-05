4fe2d3a1e344cc5ef523f6d2878bef154dcaa1df
rayong <skdyds@gmail.com>
2022-09-05 17:32:35 +0900

    Modify : 공통 Nav 컴포넌트 적용 후 코드 정리 - 이후 머지는 없다고 고려해, 기존 작업하던 스타일로 업데이트 - 로그인 페이지 상단에 고정되지 않게 컴포넌트 위치 라우터에서 메인으로 이동

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,31 +3,31 @@ import './../Nav/Nav.scss';
 
 function Nav() {
   return (
-    <nav>
-      <div className="navLeft">
-        <img src="images/instagram.png" className="instargram" alt="" />
-        <span className="fontBolder">|</span>
-        <span className="logo">Westagram</span>
-      </div>
-
-      <input className="search" type="text" placeholder="검색" />
-
-      <div className="navRight">
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
-          alt=""
-        />
-        <img
-          src=" https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-          alt=""
-        />
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
-          alt=""
-        />
-        <img src="images/profile.jpg" className="profileImage" alt="" />
-      </div>
-    </nav>
+    <div className="main">
+      <nav className="nav">
+        <div className="wrapper">
+          <div className="nav-box">
+            <div className="logo-box">
+              <img
+                className="icon"
+                src="/images/rayong/logo.png"
+                alt="westagram-logo"
+              />
+              <p>Westagram</p>
+            </div>
+            <div className="input-box">
+              <input id="search-inputbox" type="text" placeholder="검색" />
+              <i className="fa-solid fa-magnifying-glass" />
+            </div>
+            <div className="icon-box">
+              <i className="fa-regular fa-compass fa-xl" />
+              <i className="fa-regular fa-heart fa-xl" />
+              <i className="fa-regular fa-user fa-xl" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
--- src/pages/rayong/Main/Main.js
+++ src/pages/rayong/Main/Main.js
@@ -2,33 +2,12 @@ import React from 'react';
 import '../Main/Main.scss';
 import Feeds from './components/Feeds/Feeds';
 import MainRight from './components/MainRight/MainRight';
+import Nav from '../../../components/Nav/Nav';
 
 function MainRa() {
   return (
     <div className="main">
-      <nav className="nav">
-        <div className="wrapper">
-          <div className="nav-box">
-            <div className="logo-box">
-              <img
-                className="icon"
-                src="/images/rayong/logo.png"
-                alt="westagram-logo"
-              />
-              <p>Westagram</p>
-            </div>
-            <div className="input-box">
-              <input id="search-inputbox" type="text" placeholder="검색" />
-              <i className="fa-solid fa-magnifying-glass" />
-            </div>
-            <div className="icon-box">
-              <i className="fa-regular fa-compass fa-xl" />
-              <i className="fa-regular fa-heart fa-xl" />
-              <i className="fa-regular fa-user fa-xl" />
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Nav />
       <main className="contents">
         <div className="wrapper">
           <div className="contents-box">
