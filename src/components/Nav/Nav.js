5770b9656327234c93c336a75a22d5b00d9cd8f1
SungJongHwa <jonghwasung@gamail.com>
2022-09-05 16:24:16 +0900

    Nav 컴포넌트 생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import './../Nav/Nav.scss';
 
 function Nav() {
-  return <h1>Hello World</h1>;
+  return (
+    <nav>
+      <div className="navLeft">
+        <img src="images/instagram.png" className="instargram" alt="" />
+        <span className="fontBolder">|</span>
+        <span className="logo">Westagram</span>
+      </div>
+
+      <input className="search" type="text" placeholder="검색" />
+
+      <div className="navRight">
+        <img
+          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+          alt=""
+        />
+        <img
+          src=" https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+          alt=""
+        />
+        <img
+          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+          alt=""
+        />
+        <img src="images/profile.jpg" className="profileImage" alt="" />
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
