5a89925b499b43c5b761228fd637c49b60d2d768
gamangee <gamangee1991@gmail.com>
2022-09-06 17:49:46 +0900

    피드 데이터를 Mock Data로 따로 분리해준 뒤 여러개의 피드를 화면에 구현

--- src/pages/jangmoonjung/Main/Main.js
+++ src/pages/jangmoonjung/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../../../components/Nav/Nav';
 import ReplyLists from './components/ReplyLists';
 import { FOOTER_LIST } from '../Main/components/FooterList';
@@ -34,6 +34,12 @@ function MainJang() {
       setReplyValue('');
     }
   };
+  const [feedsInfoList, setFeedsInfoList] = useState([]);
+  useEffect(() => {
+    fetch('/data/feedsData.json').then(response => {
+      response.json().then(result => setFeedsInfoList(result));
+    });
+  }, []);
 
   return (
     <>
@@ -41,7 +47,7 @@ function MainJang() {
       <main className="main">
         <div className="container">
           <div className="feeds">
-            <article className="container">
+            {/* <article className="container">
               <div className="header">
                 <div className="profile">
                   <img
@@ -80,8 +86,8 @@ function MainJang() {
                     />
                   </div>
                   <div className="text">
-                    <span className="id">aineworld</span>님
-                    <span className="people">외 10명</span>이 좋아합니다
+                    <span className="id">aineworld</span>님 외
+                    <span className="people">10명</span>이 좋아합니다
                   </div>
                 </div>
                 <div className="texts">
@@ -131,7 +137,107 @@ function MainJang() {
                   게시
                 </button>
               </form>
-            </article>
+            </article> */}
+            {feedsInfoList.map(feedsInfo => {
+              return (
+                <article key={feedsInfo.id} className="container">
+                  <div className="header">
+                    <div className="profile">
+                      <img
+                        className="profile__img"
+                        src={feedsInfo.profileImg_url}
+                        alt="feeds__profile__img"
+                      />
+                      <div className="id">{feedsInfo.feedId}</div>
+                    </div>
+                    <div className="more">
+                      <i className="more__icon fas fa-ellipsis-h" />
+                    </div>
+                  </div>
+                  <img
+                    className="feeds__img"
+                    src={feedsInfo.feedImg_url}
+                    alt="feeds__img"
+                  />
+                  <div className="contents">
+                    <div className="icons">
+                      <div className="left">
+                        <i className="heart far fa-heart" />
+                        <i className="comment fa-regular fa-comment" />
+                        <i className="plane fa-regular fa-paper-plane" />
+                      </div>
+                      <div className="right">
+                        <i className="bookmark fa-regular fa-bookmark" />
+                      </div>
+                    </div>
+                    <div className="likes">
+                      <div className="image">
+                        <img
+                          className="likes__img"
+                          src={feedsInfo.likeImg_url}
+                          alt="feeds__likes__img"
+                        />
+                      </div>
+                      <div className="text">
+                        <span className="id">{feedsInfo.likeId}</span>님{' '}
+                        <span className="people">외 {feedsInfo.likeNums}</span>
+                        이 좋아합니다
+                      </div>
+                    </div>
+                    <div className="texts">
+                      <div className="content">
+                        <div className="id">{feedsInfo.feedId}</div>
+                        <div className="text" />
+                        {feedsInfo.feedContent}
+                      </div>
+                      <div className="more__gray">더보기</div>
+                    </div>
+                    <div className="newReply">
+                      <div className="texts__reply">
+                        <div className="texts__reply__left">
+                          <div className="id">{feedsInfo.replyId}</div>
+                          <div className="text">{feedsInfo.replyContent}</div>
+                        </div>
+                        <div className="texts__reply__right">
+                          <i className="texts__heart fa-regular fa-heart" />
+                          <i className="texts__remove fa-regular fa-trash-can" />
+                        </div>
+                      </div>
+                      {replyArray.map((value, index) => {
+                        return <ReplyLists key={index} content={value} />;
+                      })}
+                    </div>
+                    <div className="time">
+                      <span className="more__gray time__before">
+                        {feedsInfo.timeBefore}
+                      </span>
+                    </div>
+                  </div>
+                  <form onSubmit={replyRegist} className="reply">
+                    <input
+                      value={replyValue}
+                      onChange={e => {
+                        replyInput(e);
+                        replyValidation();
+                      }}
+                      className="input"
+                      type="text"
+                      placeholder="댓글 달기...."
+                    />
+                    <button
+                      onClick={replyRegist}
+                      disabled={replyDisable}
+                      type="button"
+                      className={
+                        replyActive ? 'textBtnActive' : 'textBtnUnactive'
+                      }
+                    >
+                      게시
+                    </button>
+                  </form>
+                </article>
+              );
+            })}
           </div>
           <aside className="main-right">
             <div className="main-right__container">
